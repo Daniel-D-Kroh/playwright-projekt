99b30ec238fd4e6053a6ad7afff78d81c00b16b9
dannydkroh <daniel.d.kroh@gmail.com>
2025-05-20 16:17:33 +0200

    playwright.config.js Jenkins config änderung

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,12 @@
 const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
-    testDir: './e2e-tests', // Dein Testordner
+    testDir: './e2e-tests',
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
-    reporter: [['list'], ['junit', { outputFile: process.env.CI ? '../test-results/playwright/playwright-results.xml' : 'results/playwright-results.xml' }]],
+    reporter: [['list'], ['junit', { outputFile: process.env.CI ? 'test-results/playwright/playwright-results.xml' : 'results/playwright-results.xml' }]],
     use: {
         trace: 'on-first-retry',
     },
